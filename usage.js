d6190c71826762e9fc7b505676b336392f01ceeb
Kris Kowal <kris@cixar.com>
2015-11-15 18:52:30 -0800

    Check in parser for speed

--- usage.js
+++ usage.js
@@ -1,18 +1,9 @@
 'use strict';
 
-var PEG = require('pegjs');
-var fs = require('fs');
-var path = require('path');
 var Result = require('rezult');
-
-var parser;
+var parser = require('./usage-parser');
 
 function parse(usage) {
-    if (!parser) {
-        var filename = path.join(__dirname, 'usage.pegjs');
-        var source = fs.readFileSync(filename, 'ascii');
-        parser = PEG.buildParser(source);
-    }
     try {
         return new Result(null, parser.parse(usage));
     } catch (error) {
